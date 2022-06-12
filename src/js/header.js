3330a165359f0bf35408edd890dcc7c374cd214f
VolodymyrKavun <92682811+VolodymyrKavun@users.noreply.github.com>
2022-06-12 16:53:02 +0300

    fixed-adaptive-and-render-btn

--- src/js/header.js
+++ src/js/header.js
@@ -1,1 +1,11 @@
+import { refs } from './refs';
+import { singleCardItem } from './searchItemList';
 
+refs.logoEl.addEventListener('click', onLogoClick);
+
+function onLogoClick() {
+  refs.container.innerHTML = '';
+  singleCardItem();
+}
+
+export { onLogoClick };

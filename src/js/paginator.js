651369bf7cc0a10410fe409fff732e85e92b6dd5
Maksimchik36 <Peltek1985@gmail.com>
2022-06-18 19:06:29 +0300

    b completed, # cann't  test

--- src/js/paginator.js
+++ src/js/paginator.js
@@ -18,7 +18,7 @@ function renderButtons(count) {
   }
   const markup = button.join('');
   return ` <li class="pager__item pager__item--prev">
-  <button class="pager__link" id="left">&#9837;
+  <button class="pager__link is-hidden" id="left">&#9837;
   </button>
     </li>
      ${markup}
@@ -81,10 +81,29 @@ async function onClickPagSearch(e) {
       refs.container.insertAdjacentHTML('beforeend', markup);
       index = res.page;
 
-      // вішаем бекграунд на поточну кнопку
+          // вішаем бекграунд на поточну кнопку
       if (index === Number(e.target.textContent)) {
         e.target.classList.add('current-accent-page');
+       
+       
+        const currentPage = document.querySelector("button.pager__link.pag.current-accent-page");
+        const prev = document.querySelector("#left");
+        const next = document.querySelector("#rigth");
+        // делаем кнопку со значком "♭" скрытой при текущей странице = 1
+        if(currentPage.dataset.page !== "1"){
+          prev.classList.remove("is-hidden");
+          } else if(!prev.classList.contains("is-hidden")){
+          prev.classList.add("is-hidden");        
+        }
+
+        // делаем кнопку со значком "#" скрытой при текущей странице = общему кол-ву страниц
+        if(currentPage.dataset.page >= res.total_pages){
+          next.classList.add("is-hidden")
+        } else if(prev.classList.contains("is-hidden")){
+          next.classList.remove("is-hidden")
+        }     
       }
+
       break;
 
     default:

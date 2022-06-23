ea86ec92daeff49e5cbaef4ac60c30cbbbe40604
myroslav <myrkozar@gmail.com>
2022-06-23 10:58:52 +0300

    up starting page

--- src/js/starting-page.js
+++ src/js/starting-page.js
@@ -5,13 +5,12 @@ function startingPage() {
   document.querySelector('body').classList.add('stop-scrolling');
 
   // // Макс тестил
-  document.querySelector('.nav-section').classList.add('hidden');  //
-  document.querySelector('.main').classList.add('hidden');  //
-  document.querySelector('.desktop').classList.add('hidden');  //
-  
+  document.querySelector('.nav-section').classList.add('hidden'); //
+  document.querySelector('.main').classList.add('hidden'); //
+  document.querySelector('.desktop').classList.add('hidden'); //
 
   document.querySelector('.video').classList.remove('hidden');
- 
+
   setTimeout(() => {
     try {
       singleCardItem();
@@ -23,8 +22,7 @@ function startingPage() {
       // // Макс тестил
       document.querySelector('.nav-section').classList.remove('hidden'); //
       document.querySelector('.main').classList.remove('hidden'); //
-      document.querySelector('.desktop').classList.remove('hidden');  //
-
+      document.querySelector('.desktop').classList.remove('hidden'); //
     }
   }, 7000);
 }
